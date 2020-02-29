72162376bed0440b59cbf057d8b4310ffdcfb897
Armen Mkrtchian <armenmkrtchian93@gmail.com>
2020-02-29 22:44:12 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-app';
+  formattedAddress = '';
+
+  public handleAddressChange(address: any) {
+  this.formattedAddress = address.formatted_address  // Do some stuff
+}
+  //AIzaSyCrcGtyBX9qgQP-W22uiapGQ-Su9zn9uV8
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { GooglePlaceModule } from "ngx-google-places-autocomplete";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GooglePlaceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
